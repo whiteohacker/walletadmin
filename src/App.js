b2e7d008edb91ff0be8baa5da196b85b553dced4
whiteohacker <58128499+whiteohacker@users.noreply.github.com>
2021-05-17 22:38:47 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import Header from "./componants/Header";
+import Menu from "./componants/Menu";
+import Content from "./componants/Content";
+import Footer from "./componants/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
